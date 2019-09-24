07a42761bac274a7cae44ad03b9cdcea91ea5d5c
h4pZ <h4pz@protonmail.com>
2019-09-24 06:52:22 +0200

    Update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,9 +9,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 5; done
 
 
 # Launch bar1 and bar2
-polybar HDMI-0-desktops -r &
-polybar HDMI-0-date -r &
-polybar HDMI-0-others -r &
-#polybar HDMI-1-spotify  -r &
-polybar HDMI-1-desktops -r &
-polybar HDMI-1-tray     -r &
+polybar HDMI-0-solid -r &
+polybar HDMI-1-solid -r &
